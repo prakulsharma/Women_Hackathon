5d76b1be776cb348bc94ca3471717c6cf5b39586
Jatin Bindra <jatinbindra171998@gmail.com>
2018-03-10 21:03:22 +0530

    dev

--- app/src/main/java/com/developer/jatin/womenhackathon/MainScreen.java
+++ app/src/main/java/com/developer/jatin/womenhackathon/MainScreen.java
@@ -33,14 +33,6 @@ CoordinatorLayout coordinatorLayout;
          toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 //        coordinatorLayout=(CoordinatorLayout)findViewById(R.id.ap)
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
