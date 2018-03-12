2430104b75c720a184636d6461d9b1588d508721
Jatin Bindra <jatinbindra171998@gmail.com>
2018-03-12 07:48:46 +0530

    dev

--- app/src/main/java/com/developer/jatin/womenhackathon/Login.java
+++ app/src/main/java/com/developer/jatin/womenhackathon/Login.java
@@ -1,13 +1,26 @@
 package com.developer.jatin.womenhackathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Login extends AppCompatActivity {
-
+ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        imageButton=(ImageButton)findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent();
+                i.setClass(Login.this,MainScreen.class);
+                startActivity(i);
+            }
+        });
     }
 }
